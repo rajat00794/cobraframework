ce52d517360705acb4a1e39db286ac3a76ce3a85
Rajat Mishra <rajatm@thoughtwin.com>
2022-11-23 18:35:08 +0530

    rfr

--- framework/create.py
+++ framework/create.py
@@ -1,42 +1,60 @@
 import os
 import json
-
+from framework.configtemplates.templateschemaloader import Template
 
 class CreateComponents:
     """_summary_"""
 
+    def file_writer(self, dataq):
+        if hasattr(dataq,"initfiles"):
+            dataq=getattr(dataq,"initfiles")
+        else:
+            dataq=dataq
+        for i in dataq:
+            with open(i.filename, "w") as ds:
+                for ui in i.content:
+                    ds.writelines(ui)
+                ds.close()
+        return True
     def create_new(self, **kwargs):
         """_summary_"""
         if "type" in kwargs.keys():
-            if kwargs["type"] != "app":
+            config=Template(name=kwargs['name'],type=kwargs["type"])
+            config=config.get_config_instances()[0]
+            if config.initfolders==[]:
                 os.chdir(os.getenv("APPROOT"))
-                os.chdir(kwargs["type"])
-                os.makedirs(kwargs["name"], exist_ok=True)
-                os.chdir(kwargs["name"])
-                if kwargs["type"].startswith("adaptors"):
-                    de = os.getenv("APPROOT")
-                    with open(
-                        f"{de}/framework/configtemplates/adaptors.json", "rb"
-                    ) as fs:
-                        dataq = json.loads(fs.read())
-                    print(dataq)
-                    for i in dataq:
-                        print(type(i), "rfrfrfr")
-                        for k, v in i.items():
-                            if k == "initfiles":
-                                for h in v:
-                                    with open(
-                                        list(h.keys())[0].format(kwargs["name"]), "w"
-                                    ) as ds:
-                                        for ui in list(h.values()):
-                                            print(ui)
-                                            map(ds.writelines, ui)
-                                        ds.close()
-
+                if config.type!="app":
+                    os.chdir(config.type)
+                    os.makedirs(config.name,exist_ok=True)
+                    os.chdir(config.name)
+                    if self.file_writer(config):
+                        return kwargs["type"] + "loaded", 200
+                else:
+                    os.chdir(os.getenv("APPPATH"))
+                    os.makedirs(kwargs["name"],exist_ok=True)
+                    os.chdir(config.name)
+                    if self.file_writer(config):
+                        return kwargs["type"] + "loaded", 200    
             else:
                 os.chdir(os.getenv("APPROOT"))
-                os.chdir(os.getenv("APPPATH"))
-                os.makedirs(kwargs["name"])
+                os.chdir(config.type)
+                os.makedirs(config.name,exist_ok=True)
+                os.chdir(config.name)
+                self.file_writer(config)
+                for i in config.initfolders:
+                    self.create_sub(i)
+            return True
+
+
+    def create_sub(self,config):
+        os.makedirs(config.name,exist_ok=True)
+        os.chdir(config.name)
+        self.file_writer(config.files)
+        sub=config.subfolders
+        if sub==[] or sub is None:
+            return "create initfolders"
+        else:
+            return list(map(self.create_sub,sub))
 
     def create_from_config(self, **kwargs):
         """_summary_"""
