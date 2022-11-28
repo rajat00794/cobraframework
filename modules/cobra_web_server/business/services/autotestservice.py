267d03056e64b76372652f5b51e527f0fe78ad84
rajat mishra <mishra.rajat855@gmail.com>
2022-11-29 01:06:08 +0530

    web_server_module

--- modules/cobra_web_server/business/services/autotestservice.py
+++ modules/cobra_web_server/business/services/autotestservice.py
@@ -8,18 +8,21 @@ class AutoTestService(ABC):
     @abc.abstractmethod
     def __init__(self, app: object, utils: object) -> None:
         pass
-
+    @abc.abstractmethod
     def load_test_conf(self) -> Dict[str, Any]:
         return
-
+    @abc.abstractmethod
     def test_routes_functions(self) -> List[function]:
         return
 
+    @abc.abstractmethod
     def test_apps_utils(self) -> Union[List[function], None]:
         return
 
+    @abc.abstractmethod
     def test_other_validations(self) -> Union[List[function], None]:
         return
 
+    @abc.abstractmethod
     def all_test_functions(self) -> List[Dict[str, List[function]]]:
         return
